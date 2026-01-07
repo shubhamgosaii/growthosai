3ee6e98c15eacf59cb7cd4d0e4e797904cac56da
Shubham Gosai <sgosain18122001@gmail.com>
2026-01-07 07:41:39 +0530

    Delete frontend directory

--- frontend/src/pages/About.tsx
+++
@@ -1,8 +0,0 @@
-import { JSX } from "react/jsx-dev-runtime";
-
-const About = (): JSX.Element => (
-  <div className="container">
-    <h1>About Us</h1>
-  </div>
-);
-export default About;
--- frontend/src/pages/Services.tsx
+++
@@ -1,8 +0,0 @@
-import { JSX } from "react/jsx-dev-runtime";
-
-const Services = (): JSX.Element => (
-  <div className="container">
-    <h1>Services</h1>
-  </div>
-);
-export default Services;
